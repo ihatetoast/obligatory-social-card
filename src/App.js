97b9c430d23abd3fe0be7bbd8052424c0b8f5592
Katy <ihatetoast@gmail.com>
2021-07-15 21:32:40 -0500

    logic finished.

--- src/App.js
+++ src/App.js
@@ -1,97 +1,31 @@
-import styled from 'styled-components'
-import ContactCard from './Components/ContactCard'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import ContactCard from "./Components/ContactCard";
 
 const Wrapper = styled.div`
-padding: 1em;
-`
-const DUMMY_PEEPS = [
-  { 
-    "gender": "male", 
-    "name": {
-      "title": "mr",
-      "first": "george",
-      "last": "fawcett"
-    },
-    "location": {
-      "street": "1245 some road",
-      "city": "austin",
-      "state": "texas",
-      "postcode": "78745"
-  },
-    "email": "george.dibbs@saluki.com",
-    "dob": {
-      "date": "2013-07-20T09:44:18.674Z",
-      "age": 7
-    },
-    "phone": "512-962-7516",
-    "cell": "512-454-0666",
-    "picture": {
-      "large": "https://randomuser.me/api/portraits/men/75.jpg",
-      "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
-      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-    },
-  },
-  { 
-    "gender": "male", 
-    "name": {
-      "title": "mr",
-      "first": "spike",
-      "last": "dibbs"
-    },
-    "location": {
-      "street": "11203 fancy street",
-      "city": "garland",
-      "state": "texas",
-      "postcode": "75040"
-  },
-    "email": "mister.dibbs@goodboi.com",
-    "dob": {
-      "date": "2014-12-03T02:20:18.674Z",
-      "age": 6
-    },
-    "phone": "214-357-5289",
-    "cell": "214-373-3793",
-    "picture": {
-      "large": "https://randomuser.me/api/portraits/men/15.jpg",
-      "medium": "https://randomuser.me/api/portraits/med/men/15.jpg",
-      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/15.jpg"
-    },
-  },
-  { 
-    "gender": "female", 
-    "name": {
-      "title": "ms",
-      "first": "haddy",
-      "last": "pants"
-    },
-    "location": {
-      "street": "7377 foster avenue",
-      "city": "dallas",
-      "state": "texas",
-      "postcode": "75229"
-  },
-    "email": "haddy.haddypants@fosters.com",
-    "dob": {
-      "date": "2018-03-15T19:23:18.674Z",
-      "age": 3
-    },
-    "phone": "214-739-0883",
-    "cell": "214-521-4789",
-    "picture": {
-      "large": "https://randomuser.me/api/portraits/women/10.jpg",
-      "medium": "https://randomuser.me/api/portraits/med/women/10.jpg",
-      "thumbnail": "https://randomuser.me/api/portraits/thumb/women/10.jpg"
-    },
-  }
-]
+  padding: 1em;
+`;
 
 const App = () => {
- 
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=9")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setContacts(data.results);
+      });
+  }, []);
+
   return (
     <Wrapper>
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
+      {contacts.map((contact) => (
+        <ContactCard
+          avatar={contact.picture.large}
+          age={contact.dob.age}
+          name={contact.name.last + ", " + contact.name.first}
+        />
+      ))}
     </Wrapper>
   );
 };
--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Card from "../UI/Card";
+
 const ContactCard = (props) => {
     const [showAge, setShowAge] = useState(false);
     const handleShowAge =()=>{
@@ -8,11 +9,11 @@ const ContactCard = (props) => {
   return (
     <Card>
       <div className="card">
-        <img src="http://placekitten.com/150/150" alt="kitten headshot" />
-        <p>Name: Dude McTude</p>
-        <p>email: dude@mctude.com</p>
+        <img src={props.avatar} alt="kitten headshot" />
+        <p>Name: {props.name}</p>
+        <p>email: {props.email}</p>
         <button onClick={handleShowAge}>{showAge === true ? "Hide age" : "Show age"}</button>
-        {showAge === true ? <p>Age: 25</p> : null}
+        {showAge === true ? <p>Age: {props.age}</p> : null}
       </div>
     </Card>
   );
