8420bd8b7fb5ef2a8c2f8552f8ed5c7196bf5eb9
Katy <ihatetoast@gmail.com>
2021-07-29 23:35:54 -0500

    styling hover to show detail. in prog. hairpulled

--- src/UI/IconWrapper.js
+++ src/UI/IconWrapper.js
@@ -1,26 +1,36 @@
 import React from "react";
 import styled from "styled-components";
-
 const StyledListItem = styled.li`
+  background: #fff;
   display: inline-block;
   position: relative;
   width: 30px;
   height: 30px;
-  &:after{
-    ${'' /* content: "I am content for styling"; */}
-    background: #fff;
-    position: absolute;
-
+  img {
+    width: 100%;
   }
-
-  
 `;
-// ON HOVER, SHOW THE CONTENT AS BEFORE OR AFTER`
-const StyledIcon = styled.img`
-width: 100%;
-`
+
+// UP NEXT: CENTER THE INFO DIV BELOW THE ICON. ADD TRIANGLE/ROTATED SQUARE
+const StyledDetail = styled.div`
+  background-color: #fff;
+  border: 1px solid #1f4f6f;
+  box-shadow: 0 1px 2px rgba(31, 79, 111, 0.25);
+  display: none;
+  padding: 5px 10px;
+  position: absolute;
+  white-space: nowrap;
+  ${StyledListItem}:hover & {
+    display: block;
+  }
+`;
 const IconWrapper = (props) => {
   const classes = "icon-wrapper " + props.className;
-  return <StyledListItem className={classes}><StyledIcon src={props.imgSrc} alt="icon"/> </StyledListItem>
+  return (
+    <StyledListItem className={classes}>
+      <img src={props.imgSrc} alt="icon" />
+      <StyledDetail>{props.hoverContent}</StyledDetail>
+    </StyledListItem>
+  );
 };
 export default IconWrapper;
